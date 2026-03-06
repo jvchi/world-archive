e05bd870108346d202d3fbfd6863bc33d74dbf8b
jvchi <elechidestinyj@gmail.com>
2026-03-06 19:27:42 +0100

    omo lost again,

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,30 +3,41 @@ import { useState, useEffect } from 'react'
 
 export default function () {
 
-  const [galleryData, setGalleryData] = useState([
-    // {id: 1, name: 'Van Gogh', imageURL: 'www.ancibac.cioom'},
-    // {id: 2, name: 'Van Gogh', imageURL: 'www.ancibac.cioom'},
-    // {id: 3, name: 'Van Gogh', imageURL: 'www.ancibac.cioom'},
-    // {id: 4, name: 'Van Gogh', imageURL: 'www.ancibac.cioom'},
-    // {id: 5, name: 'Van Gogh', imageURL: 'www.ancibac.cioom'},
-  ])
+  const [galleryData, setGalleryData] = useState([])
+  const [objectIds, setObjectIds] = useState()
+
   
-  const randomNumber = Math.floor(Math.random() * galleryData.length)
+  // const randomNumber = Math.floor(Math.random() * galleryData.length)
 
   useEffect(()=>{
-    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomNumber}`)
+    fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects')
       .then(res => res.json())
-      .then(data => setGalleryData(prev =>({
-        ...prev, ...data
-      })))
+      .then(id => {
+        const first100 = id.objectIDs.slice(0, 100)
+        setObjectIds(first100)
+
+        return Promise.all(
+        first100.map(id=>(
+          fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects${id}`)
+          .then(res => res.json)
+        ))
+      )
+      })
+
+      .then(artworks => {
+        const validImages = artworks.filter(a => a.primaryImage)
+        setGalleryData(validImages)
+        console.log(validImages)
+      })
+      
   }, [])
 
-  console.log(galleryData)
 
   const galleryElements = galleryData.map(data=>(
-    <span>
-      <img src={data.imageURL} alt={data.name} />
-      <h3>{data.name}</h3>
+   <span key={data.objectID}>
+      <img src={data.primaryImage} alt={data.title} />
+      <h3>{data.title}</h3>
+      <p>{data.artistDisplayName}</p>
     </span>
   ))
 
