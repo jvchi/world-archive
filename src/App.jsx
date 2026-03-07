8393147f11eb60ba5be1d08a4410ae76f37a6ea6
jvchi <elechidestinyj@gmail.com>
2026-03-07 20:34:14 +0100

    biko save

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import Header from "./components/header.jsx"
-import Art from "./components/art.jsx"
-import Footer from "./components/footer.jsx"
 import Data from "./components/data.js"
 import Carousel from "./components/Carousel.jsx"
 import Frame from "./components/Frame.jsx"
